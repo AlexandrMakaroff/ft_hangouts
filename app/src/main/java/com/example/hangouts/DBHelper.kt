fb60496487f2803a8776e45939ffaec906ef0e5c
Festus Georgann <fgeorgan@et-b5.kzn.21-school.ru>
2022-03-22 19:02:29 +0300

    interface

---
+++ app/src/main/java/com/example/hangouts/DBHelper.kt
@@ -0,0 +1,25 @@
+package com.example.hangouts
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+const val DATABASE_VERSION = 1
+const val DATABASE_NAME = "contactDB"
+const val TABLE_CONTACTS = "contacts"
+
+const val KEY_ID = "_id"
+const val KEY_NAME = "name"
+const val KEY_PHONE = "phone"
+
+class DBHelper(context: Context?) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("create table $TABLE_CONTACTS ($KEY_ID integer primary key,$KEY_NAME text,$KEY_PHONE text)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("drop table if exists $TABLE_CONTACTS")
+        onCreate(db)
+    }
+}
--- app/src/main/java/com/example/hangouts/MainActivity.kt
+++ app/src/main/java/com/example/hangouts/MainActivity.kt
@@ -2,10 +2,75 @@ package com.example.hangouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.hangouts.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var buttonAdd: Button
+    private lateinit var buttonRead: Button
+    private lateinit var buttonEdit: Button
+    private lateinit var buttonDelete: Button
+
+    private lateinit var editTextName: EditText
+    private lateinit var editTextPhoneNumber: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        editTextName = binding.editTextName
+        editTextPhoneNumber = binding.editTextPhoneNumber
+
+        buttonAdd = binding.buttonAdd
+        buttonRead = binding.buttonRead
+        buttonEdit = binding.buttonEdit
+        buttonDelete = binding.buttonDelete
+
+        buttonAdd.setOnClickListener(this)
+        buttonRead.setOnClickListener(this)
+        buttonEdit.setOnClickListener(this)
+        buttonDelete.setOnClickListener(this)
+
+        val dbHelper = DBHelper(this)
+    }
+
+    private fun firstBut(view: View)
+    {
+        // выводим сообщение
+        Toast.makeText(this, "Первая Кнопка", Toast.LENGTH_SHORT).show();
+    }
+
+    private fun secondBut(view: View)
+    {
+        // выводим сообщение
+        Toast.makeText(this, "Вторая Кнопка", Toast.LENGTH_SHORT).show();
+    }
+
+    private fun thirdBut(view: View)
+    {
+        // выводим сообщение
+        Toast.makeText(this, "Третья Кнопка", Toast.LENGTH_SHORT).show();
+    }
+
+    override fun onClick(v: View?) {
+        val name = editTextName.text.toString()
+        val phone = editTextPhoneNumber.text.toString()
+
+        if (v != null) {
+            when (v.id) {
+                buttonAdd.id -> firstBut(v)
+                buttonRead.id -> secondBut(v)
+                buttonEdit.id -> thirdBut(v)
+                buttonDelete.id -> thirdBut(v)
+            }
+        }
     }
 }
